937c4a426ce4b83258866f53ebc60db9cc73befa
Piotrek <p.delkowski@gmail.com>
2018-06-16 02:38:03 +0200

    Updated API to https

--- src/js/presale.js
+++ src/js/presale.js
@@ -22,7 +22,7 @@
 
       $.ajax({
           type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
-          url: 'http://panel.orionvault.com/api/v1/presales/', // the url where we want to POST
+          url: 'https://panel.orionvault.com/api/v1/presales/', // the url where we want to POST
           data: formData, // our data object
           dataType: 'json', // what type of data do we expect back from the server
           encode: true
