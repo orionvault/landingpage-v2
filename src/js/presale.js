c69d4ab79a4eb8ff62b156a51acb3000bbcb93e7
Piotrek <p.delkowski@gmail.com>
2018-07-20 19:54:05 +0200

    Compiles js files

--- src/js/presale.js
+++ src/js/presale.js
@@ -24,7 +24,7 @@
 
       $.ajax({
           type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
-          url: 'https://panel.orionvault.com/api/v1/presales/'
+          url: 'https://panel.orionvault.com/api/v1/presales/',
           data: formData, // our data object
           dataType: 'json', // what type of data do we expect back from the server
           encode: true
